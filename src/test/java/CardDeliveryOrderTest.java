bf17f21e3c850abe0f5813925ba961e4a4ab55b4
Vadim Piskarev <woxadestroyer@gmail.com>
2022-08-19 19:03:46 +0300

    tests added

--- src/test/java/CardDeliveryOrderTest.java
+++ src/test/java/CardDeliveryOrderTest.java
@@ -171,7 +171,7 @@ public class CardDeliveryOrderTest {
     }
 
     @Test
-    void shouldFillTheFormWithNorma() {
+    void shouldFillTheFormWithoutACheckBox() {
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Казань");
         $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.BACK_SPACE, date(3));
